ef3e565b0c6e6ca6572cd422a645704c23117553
juanma1231 <juan.zuluaga4461@uco.net.co>
2022-11-01 15:44:55 -0500

    primeros avances

--- app/backend/src/com/myproyect/solveit/domain/domain/PersonDomain.py
+++ app/backend/src/com/myproyect/solveit/domain/domain/PersonDomain.py
@@ -1,12 +1,14 @@
+from selectors import SelectorKey
 import string
 from tokenize import String
+from uuid import UUID
 
 from app.backend.src.com.myproyect.solveit.domain.domain.IdTypeDomain import IdTyopeDomain, IdTypeDomain
 from app.backend.src.com.myproyect.solveit.domain.domain.ZoneDomain import ZoneDomain
 
 
 class PersonaDomain:
-    id=int
+    id= UUID
     name= string
     lastName= string
     idNumber= int
@@ -16,8 +18,38 @@ class PersonaDomain:
     description=string
     zone= ZoneDomain()
     password = string
-
-    def setName(self)
+    #SETTERS
+    def setName(self, name):
+        self.name = name
+    def setId(self, id):
+        self.id = id
+    def setLastName(self, lastName):
+        self.lastName = lastName
+    def setIdType(self, idType):
+        self.idType = idType
+    def setNumber(self, number):
+        self.number = number
+    def setMail(self, mail):
+        self.mail = mail
+    def setDescription(self, description):
+        self.description = description
+    def setZone(self, zone):
+        self.zone = zone
+    def setPassword(self, password):
+        self.password = password
+    
+    #GETTERS
+    def getId(self):
+        return self.id
+    def getName(self):
+        return self.name
+    def getLastName(self):
+        return self.lastName
+    def getIdType(self):
+        return self.idType
+    def getNumbe(self):
+        return self.number
+    def 
 
 
 
