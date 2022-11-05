7c6e31b0c8abfae77a4a9bbd964843761d181c32
Juan Pablo Morales Gomez <84155698+JuanMorales4619@users.noreply.github.com>
2022-11-05 18:42:11 -0500

    Front

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -3,21 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginButton from './com/myproject/solveit/component/Button/Login/LoginButton';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import CreatePublication from './com/myproject/solveit/component/pages/CreatePublication'
+import LogoutButton from './com/myproject/solveit/component/Button/Login/LogoutButton';
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 function App() {
-  
+  const { isAuthenticated} = useAuth0();
   return (
     
     <div className='App'>
-        
-      
       <BrowserRouter>
         <Routes>
-            <Route  path='/' element = {<LoginButton/>}/>
-            <Route  path='/home' element = {<CreatePublication/>}/>
+            <Route  path='/product/regist' element = {<CreatePublication/>}/>
+            <Route  path='*' element = {<h1>Zona de inicio</h1>}/>
           </Routes>
       </BrowserRouter>
 
+      {
+         isAuthenticated ? <LoginButton/> : <LogoutButton/>
+      }
     </div>
   
   );
