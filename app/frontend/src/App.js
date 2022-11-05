a618e5d24f9795e3a5e369114aa66b2b21d0242d
Juan Pablo Morales Gomez <84155698+JuanMorales4619@users.noreply.github.com>
2022-11-05 18:23:31 -0500

    Front

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginButton from './com/myproject/solveit/component/Button/Login/LoginButton';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import CreatePublication from './com/myproject/solveit/component/pages/CreatePublication'
 
 function App() {
   
-
   return (
+    
     <div className='App'>
-      <h1>Application</h1>
-      <LoginButton/>
+        
+      
+      <BrowserRouter>
+        <Routes>
+            <Route  path='/' element = {<LoginButton/>}/>
+            <Route  path='/home' element = {<CreatePublication/>}/>
+          </Routes>
+      </BrowserRouter>
+
     </div>
   
   );
 }
 
 export default App;
+
