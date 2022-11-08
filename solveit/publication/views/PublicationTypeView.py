50f8ae7923d5616bda434ce82adcb9cee1bb3c64
juanma1231 <juan.zuluaga4461@uco.net.co>
2022-11-08 11:57:35 -0500

    en views

--- solveit/publication/views/PublicationTypeView.py
+++ solveit/publication/views/PublicationTypeView.py
@@ -21,27 +21,25 @@ class PublicationTypeView:
         else:
             publicationType = list(Publication_Type.objects.values())
             if len(publicationType)>0:
-                dates = {'message':'Success','zones': publicationType}
+                dates = {'message':'Success','publicationType': publicationType}
             else:
-                dates = {'message':'zones not found...'}
+                dates = {'message':'publicationType not found...'}
             return JsonResponse(dates)
         
     def post(self,request):
         jd = json.loads(request.body)
-        Publication_Type.objects.create(description=jd['description'],corregimiento=jd['corregimiento_id'])
-        corregimeinto_id_id= jd['corregimiento_id']
+        Publication_Type.objects.create(description=jd['description'])
         dates = {'message':'Success'}
         return JsonResponse(dates)
 
 
     def put(self,request,id):
         jd = json.loads(request.body)
-        zones =list(Publication_Type.objects.filter(id=id).values())
-        if len(zones)>0:
-           zones = Publication_Type.objects.get(id = id)
-           zones.description=jd['description']
-           zones.corregimiento_id=jd['corregimeinto_id']
-           zones.save()
+        publicationType =list(Publication_Type.objects.filter(id=id).values())
+        if len(publicationType)>0:
+           publicationType = Publication_Type.objects.get(id = id)
+           publicationType.description=jd['description']
+           publicationType.save()
            dates ={'message':'Success'}
             
         else:
@@ -49,10 +47,10 @@ class PublicationTypeView:
         return JsonResponse(dates)
 
     def delete(self,request,id):
-        zones =list(Publication_Type.objects.filter(id=id).values())
-        if len(zones)>0:
+        publicationType =list(Publication_Type.objects.filter(id=id).values())
+        if len(publicationType)>0:
             Publication_Type.objects.filter(id=id).delete()
             dates ={'message':'Success'}
         else:
-            dates = {'message':'zones not found...'}
+            dates = {'message':'publicationType not found...'}
         return JsonResponse(dates)
