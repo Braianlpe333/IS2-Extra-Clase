635297e4cb03d9f50403c442bc7b3b160a09b9b4
juanma1231 <juan.zuluaga4461@uco.net.co>
2022-11-08 11:28:27 -0500

    ZoneView

--- solveit/publication/views/PublicationTypeView.py
+++ solveit/publication/views/PublicationTypeView.py
@@ -1,33 +1,34 @@
 from django.views import View
-from ..models import Zone
+from ..models import Publication_Type
 from django.http.response import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 import json
-class ZoneView:
+
+class PublicationTypeView:
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
     def get(self,request,id = 0):
         if(id>0):
-            zones= list(Zone.objects.filter(id=id).values())
-            if len(zones) >0:
-                dates = {'message':'Success','zones': zones}
+            publicationType= list(Publication_Type.objects.filter(id=id).values())
+            if len(publicationType) >0:
+                dates = {'message':'Success','publicationType': publicationType}
             else:
-                dates = {'message':'zones not found...'}
+                dates = {'message':'publicationType not found...'}
             return JsonResponse(dates)
         else:
-            zones = list(Zone.objects.values())
-            if len(zones)>0:
-                dates = {'message':'Success','zones': zones}
+            publicationType = list(Publication_Type.objects.values())
+            if len(publicationType)>0:
+                dates = {'message':'Success','zones': publicationType}
             else:
                 dates = {'message':'zones not found...'}
             return JsonResponse(dates)
         
     def post(self,request):
         jd = json.loads(request.body)
-        Zone.objects.create(description=jd['description'],corregimiento=jd['corregimiento_id'])
+        Publication_Type.objects.create(description=jd['description'],corregimiento=jd['corregimiento_id'])
         corregimeinto_id_id= jd['corregimiento_id']
         dates = {'message':'Success'}
         return JsonResponse(dates)
@@ -35,9 +36,9 @@ class ZoneView:
 
     def put(self,request,id):
         jd = json.loads(request.body)
-        zones =list(Zone.objects.filter(id=id).values())
+        zones =list(Publication_Type.objects.filter(id=id).values())
         if len(zones)>0:
-           zones = Zone.objects.get(id = id)
+           zones = Publication_Type.objects.get(id = id)
            zones.description=jd['description']
            zones.corregimiento_id=jd['corregimeinto_id']
            zones.save()
@@ -48,9 +49,9 @@ class ZoneView:
         return JsonResponse(dates)
 
     def delete(self,request,id):
-        zones =list(Zone.objects.filter(id=id).values())
+        zones =list(Publication_Type.objects.filter(id=id).values())
         if len(zones)>0:
-            Zone.objects.filter(id=id).delete()
+            Publication_Type.objects.filter(id=id).delete()
             dates ={'message':'Success'}
         else:
             dates = {'message':'zones not found...'}
